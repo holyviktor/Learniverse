0c4b947e3829b4d5c48e42a2fe88c067ec7162f3
Ivanka-karaim <karaimivanna@gmail.com>
2023-03-10 21:02:44 +0200

    teacher_student db

--- student/models.py
+++ student/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_lenght=40)
+    surname = models.CharField(max_lenght=40)
+    email = models.EmailField(max_length=100, unique=True)
+    date_birth = models.DateField(blank=True)
+    password = models.CharField(max_length=100)
+    phone_number = models.CharField(max_lenght=20)
+
--- teacher/models.py
+++ teacher/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Teacher(models.Model):
+    name = models.CharField(max_lenght=40)
+    surname = models.CharField(max_lenght=40)
+    email = models.EmailField(max_length=100, unique=True)
+    date_birth = models.DateField(blank=True)
+    password = models.CharField(max_length=100)
+    phone_number = models.CharField(max_lenght=20)
+    experience = models.IntegerField()
+    description = models.TextField(max_lenght=500)
+
