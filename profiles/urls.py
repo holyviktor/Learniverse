b3adb2ca9db015a194c5d3213cd5d72e7996293c
Ivanka-karaim <karaimivanna@gmail.com>
2023-03-27 20:20:55 +0300

    changed urls and views in profiles

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,26 +3,16 @@ from django.urls import path, include
 
 from profiles import views
 
-# from tours.views import tours
-# from cart.views import cart
 
 urlpatterns = [
     path('', views.profiles_index),
     path('register', views.profiles_register),
     path('login', views.profiles_login),
-]
-
-urlpatterns = [
-    path('profile', views.teacher_profile),
-    path('courses', views.teacher_courses),
-    path('course/<int:id_course>', views.teacher_course_id),
+    path('courses', views.user_courses),
+    path('course/<int:id_course>', views.user_course_id),
     path('add_course', views.teacher_add_courses),
     path('delete_course/<int:id_course>', views.teacher_delete_id),
-]
-
-
-urlpatterns = [
-    path('profile', views.student_profiles),
     path('wishlist', views.student_wishlist),
-    path('courses', views.student_courses),
+
 ]
+
