d838b205072fd2b5f030ed2195cae4cedebf18f8
Ivanka-karaim <karaimivanna@gmail.com>
2023-03-29 17:13:30 +0300

    changed views and created templates in profile

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,14 +5,14 @@ from profiles import views
 
 
 urlpatterns = [
-    path('', views.profiles_index),
+    path('', views.profiles_index, name="profile"),
     path('register', views.profiles_register),
     path('login', views.profiles_login),
+    path('logout', views.profiles_logout),
     path('courses', views.user_courses),
     path('course/<int:id_course>', views.user_course_id),
     path('add_course', views.teacher_add_courses),
     path('delete_course/<int:id_course>', views.teacher_delete_id),
-    path('wishlist', views.student_wishlist),
-
+    path('wishlist', views.student_wishlist)
 ]
 
