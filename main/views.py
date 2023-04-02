2346653b4b31b616f441ac1842db53d58cf6dfb1
dianakrupko <dnkrupkoo@gmail.com>
2023-04-02 14:09:54 +0300

    changes in templates

--- main/views.py
+++ main/views.py
@@ -1,11 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
-
+from courses.models import Category, Course, Module, Lection, Test, Question, Answer
 # Create your views here.
 def main_index(request):
-
-    return HttpResponse("main")
+    courses = Course.objects.filter()
+    return render(request, 'index.html', context={"courses": courses})
 
 
 def main_about(request):
