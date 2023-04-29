5bc140841e6210bb384d0854757ff253d5f50269
Victoria Fox <83537604+holyviktor@users.noreply.github.com>
2023-04-29 12:58:12 +0300

    form for enrollment for course and deleting from course on courses pages

--- courses/views.py
+++ courses/views.py
@@ -13,24 +13,26 @@ def enroll_course(request):
     print("here?")
     if form_enroll.is_valid():
         if form_enroll.cleaned_data['course_enroll']:
-            print("enroll")
+
+            # print("enroll")
             now = datetime.now()
             course_id = form_enroll.cleaned_data['course_enroll']
             user_id = request.session['user_id']
             user = User.objects.get(id=user_id)
 
-            print(user.id)
+            # print(user.id)
             course = Course.objects.get(id=course_id)
             user_course = UserCourse.objects.filter(user_id=user, course_id=course)
             if not user_course:
-                print(course.id)
-                print(now.strftime("%Y/%m/%d %H:%M:%S"))
+                # print(course.id)
+                # print(now.strftime("%Y/%m/%d %H:%M:%S"))
                 a = UserCourse(date_start=now.strftime("%Y-%m-%d %H:%M"), user=user, course=course)
                 a.save()
-                return redirect('profile')
+                form_enroll.cleaned_data['course_enroll'] = None
     if form_delete.is_valid():
         if form_delete.cleaned_data['course_delete']:
-            print("delete")
+
+            # print("delete")
             course_id = form_delete.cleaned_data['course_delete']
             user_id = request.session['user_id']
             user = User.objects.get(id=user_id)
@@ -40,16 +42,29 @@ def enroll_course(request):
             print(user_course)
             if user_course:
                 user_course.delete()
-                return redirect('profile')
+                form_delete.cleaned_data['course_delete'] = None
+                # return redirect('profile')
 
 
 # Create your views here.
 def courses_index(request):
     courses = Course.objects.filter()
+    user_id = request.session['user_id']
     if request.method == 'POST':
         enroll_course(request)
+    show_course_enroll = {}
+    for course in courses:
+        if user_id:
+            user_course = UserCourse.objects.filter(user_id=user_id, course_id=course.id)
+            if user_course:
+                show_course_enroll[course] = False
+            else:
+                show_course_enroll[course] = True
+        else:
+            show_course_enroll[course] = False
+    print(show_course_enroll)
     # return render(request, 'login.html', {'form': form})
-    return render(request, 'courses.html', context={"courses": courses})
+    return render(request, 'courses.html', context={"courses": show_course_enroll})
 
 
 def courses_search(request, name):
@@ -80,15 +95,14 @@ def courses_id_module_id_lecture(request, id, id_module, id_lecture):
 def courses_id(request, id):
     course = Course.objects.filter(id=id)
     user_id = request.session['user_id']
+
+    if request.method == 'POST':
+        enroll_course(request)
     if user_id:
         show_enroll = True
-        user = User.objects.get(id=user_id)
         user_course = UserCourse.objects.filter(user_id=user_id, course_id=id)
         if user_course:
             show_enroll = False
-    print(course)
-    if request.method == 'POST':
-        enroll_course(request)
     if course:
         modules = Module.objects.filter(course_id=course[0].id)
         return render(request, 'course.html', context={"course": course[0], "modules": modules, "show_enroll":show_enroll})
