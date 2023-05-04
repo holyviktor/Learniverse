683514037235b1d2f21942e0c8743f635fd8c5a6
dianakrupko <dnkrupkoo@gmail.com>
2023-05-04 22:47:51 +0300

    new commit

--- courses/urls.py
+++ courses/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('<int:id>', views.courses_id, name="course"), #
     path('search/<str:name>', views.courses_search),
     path('category/<str:name>', views.courses_category), #
-    path('<int:id>/modules', views.course_modules),
-    path('<int:id>/module/<int:id_module>', views.courses_id_module_id), #
-    path('<int:id>/module/<int:id_module>/lecture/<int:id_lecture>', views.courses_id_module_id_lecture),
+    path('<int:id>/modules', views.course_modules, name="modules"),
+    path('<int:id>/module/<int:id_module>', views.courses_id_module_id, name="module"), #
+    path('<int:id>/module/<int:id_module>/lecture/<int:id_lecture>', views.courses_id_module_id_lecture, name="lecture"),
     path('<int:id>/module/<int:id_module>/test/<int:id_test>', views.courses_id_module_id_test),#
 ]
